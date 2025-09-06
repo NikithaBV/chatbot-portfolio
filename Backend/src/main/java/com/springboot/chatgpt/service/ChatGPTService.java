3f934f54788aafcb20b409d001211f4c7e6f8742
Nikitha <112423869+NikithaBV@users.noreply.github.com>
2025-09-06 15:22:44 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/springboot/chatgpt/service/ChatGPTService.java
@@ -0,0 +1,64 @@
+package com.springboot.chatgpt.service;
+
+
+import com.springboot.chatgpt.dto.ChatGPTRequest;
+import com.springboot.chatgpt.dto.ChatGPTResponse;
+import com.springboot.chatgpt.dto.PromptRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class ChatGPTService  {
+
+    private final RestClient restClient;
+
+    public ChatGPTService(RestClient restClient){
+        this.restClient = restClient;
+    }
+
+    //fetching value from application.properties
+    @Value("${openapi.api.key}")
+    private String apiKey;
+
+    @Value("${openapi.api.model}")
+    private String model;
+
+//    public String getChatResponse(PromptRequest promptRequest){
+//
+//        ChatGPTRequest chatGPTRequest = new ChatGPTRequest(
+//                model,
+//                List.of(new ChatGPTRequest.Message("user",promptRequest.prompt()))
+//        );
+//
+//        ChatGPTResponse response = restClient.post()
+//                .header("Authorization","Bearer " + apiKey)
+//                .header("Content-Type","application/json")
+//                .body(chatGPTRequest)
+//                .retrieve()
+//                .body(ChatGPTResponse.class);
+//
+//        return response.choices().get(0).message().content();
+//    }
+
+    public String getChatResponse(PromptRequest promptRequest){
+
+        ChatGPTRequest chatGPTRequest = new ChatGPTRequest(
+                model,
+                List.of(new ChatGPTRequest.Message("user", promptRequest.prompt()))
+        );
+
+        ChatGPTResponse response = restClient.post()
+                .header("Authorization","Bearer " + apiKey)
+                .header("Content-Type","application/json")
+                .body(chatGPTRequest)
+                .retrieve()
+                .body(ChatGPTResponse.class);
+
+        // Return as-is (keep Markdown formatting)
+        return response.choices().get(0).message().content();
+    }
+
+}
